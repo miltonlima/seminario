8e3bbfa9298afabb06a59d36974d1a102036ae7e
Milton Lima <milton.mlima@gmail.com>
2022-10-14 08:45:39 -0300

    Preparando insert

--- cadastra.php
+++ cadastra.php
@@ -1,8 +1,15 @@
 <?php
+$k = '';
+$v = '';
+foreach ($_POST as $key => $value) {
+  //echo "POST parameter '$key' has '$value'";
 
-foreach($_POST as $key => $value) {
-    //echo "POST parameter '$key' has '$value'";
-    echo "POST parameter '$key' has '$value'<br>";
-    $k = $key
-  }
+  echo "POST parameter '$key' has '$value'<br>";
+  $k .=  $key . ", ";
+  $v .=  $value . ", ";
+}
 
+$k .= "data_registro";
+$v .= "CURRENT_TIMESTAMP";
+
+$sql = "select * from inscricao_seminario ($k) values($v)"
